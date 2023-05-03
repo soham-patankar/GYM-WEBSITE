13d063db222cba4755acc67cd59eda5adc7d3762
soham-patankar <132045264+soham-patankar@users.noreply.github.com>
2023-05-03 18:09:22 +0530

    Add files via upload

---
+++ Gym Website/gymhome.js
@@ -0,0 +1,15 @@
+function login(){
+    document.getElementById('login-popup-window').style.display="block";
+}
+
+function closelogin() {
+document.getElementById('login-popup-window').style.display="none";
+}
+
+function signup(){
+    document.getElementById('signup-popup-window').style.display="block";
+}
+
+function closesignup() {
+document.getElementById('signup-popup-window').style.display="none";
+}
